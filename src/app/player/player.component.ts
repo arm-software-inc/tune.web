3205d921ce095d6921b7fb26df5d0707cfcb6051
abjesus <abel.jesus.silva@outlook.com>
2022-01-08 02:44:03 -0300

    feat: play and pause

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { Radio } from '../models/radio';
 
 @Component({
@@ -6,9 +6,15 @@ import { Radio } from '../models/radio';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.sass']
 })
-export class PlayerComponent implements OnChanges {
+export class PlayerComponent implements OnInit, OnChanges {
   @Input() selectedStation: Radio | undefined;
   @ViewChild('audioPlayer', { static: true }) audioPlayer!: ElementRef<HTMLAudioElement>; 
+
+  playing: boolean = false;
+
+  ngOnInit(): void {
+    this.registerAudioEvents();
+  }  
 
   // when station changes
   ngOnChanges(changes: SimpleChanges): void {
@@ -20,6 +26,20 @@ export class PlayerComponent implements OnChanges {
     this.audioPlayer.nativeElement.play()
       .catch((err: DOMException) => {
         console.warn('Usuário não interagiu com a aplicação', err);
-      }); 
+      });
+  }
+
+  registerAudioEvents(): void {
+    this.audioPlayer.nativeElement.addEventListener("play", () => {
+      this.playing = true;
+    });
+
+    this.audioPlayer.nativeElement.addEventListener("pause", () => {
+      this.playing = false;
+    });
+  }
+
+  play() {    
+    this.playing ? this.audioPlayer.nativeElement.pause() : this.audioPlayer.nativeElement.play()
   }
 }
