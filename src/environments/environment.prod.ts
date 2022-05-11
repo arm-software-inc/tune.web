938222859bb28e2eaca45a4dfdbaadd513d9865a
ropoko <rodrigostramantinoli@gmail.com>
2022-05-11 12:39:44 -0300

    feat: changed to nextjs

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  stations: 'http://all.api.radio-browser.info/json/stations?offset=0&limit=10'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  stations: 'http://all.api.radio-browser.info/json/stations?offset=0&limit=30'
-};
